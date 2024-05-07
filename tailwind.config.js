b8a924bdf59f563f6cc6d49c1c0e2ba9cd42b1cb
Blake Green <blake.green@littlerocket.io>
2024-05-07 12:52:58 -0400

    rebuild - 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 import remToPxPlugin from 'tailwindcss-rem-to-px';
-
 export default {
-    content: [
-      "./**/*.{js,json,liquid}",
-    ],
-    theme: {
-      screens: {
-        'md': '750px',
-        'lg': '990px',
-      },
-      extend: {
-        fontFamily: {
-          'heading': 'var(--font-heading-family)'
-        },
+  prefix: 'tw-',
+  important: true,
+  content: [
+    "./**/*.{js,json,liquid}",
+  ],
+  theme: {
+    screens: {
+      'md': '750px',
+      'lg': '990px',
+    },
+    extend: {
+      fontFamily: {
+        'heading': 'var(--font-heading-family)'
       },
     },
-    plugins: [
-      remToPxPlugin({
-        baseFontSize: 16,
-      })
-    ],
-  }
+  },
+  corePlugins: {
+    preflight: false,
+ },
+  plugins: [
+    remToPxPlugin({
+      baseFontSize: 16,
+    })
+  ],
+}
